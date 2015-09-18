ab70470e3fe9809ca6014781940b8ab5aa44f4e7
Jeffrey Mealo <jeffreymealo@gmail.com>
2015-09-17 21:00:04 -0400

    Add unit tests for successful loading of configuration file and create forbidden.json at run-time since we can't commit a file with 000 permissions

--- test/cli.test.js
+++ test/cli.test.js
@@ -1,46 +1,62 @@
 var assert = require('assert'),
     Lapidus = require('../index.js'),
-    spawnSync = require('child_process').spawnSync;
+    spawnSync = require('child_process').spawnSync,
+    fs = require('fs');
 
 Lapidus.loadConfig('./a-way-off-the-island.json');
 
-describe('CLI', function () {
-    describe('Configuration File', function () {
+describe('Configuration File', function () {
 
-        it('returns a helpful error when the file does not exist', function (done) {
-            Lapidus.loadConfig('./test/config/a-way-off-the-island.json', function (err) {
-                var output = spawnSync('node', ['index.js', '-c', './test/config/a-way-off-the-island.json']);
+    it('returns a helpful error when the file does not exist', function (done) {
+        Lapidus.loadConfig('./test/config/a-way-off-the-island.json', function (err) {
+            var output = spawnSync('node', ['index.js', '-c', './test/config/a-way-off-the-island.json']);
 
-                assert.notEqual(err, null);
-                assert.notEqual(output.stderr.toString().indexOf(err.message), -1);
-                assert.notEqual(output.status, 0);
+            assert.notEqual(err, null);
+            assert.notEqual(output.stderr.toString().indexOf(err.message), -1);
+            assert.notEqual(output.status, 0);
 
-                done();
-            });
-        });
-
-        it('returns a helpful error when we have insufficient permissions', function (done) {
-            Lapidus.loadConfig('./test/config/forbidden.json', function (err) {
-                var output = spawnSync('node', ['index.js', '-c', './test/config/forbidden.json']);
-
-                assert.notEqual(err, null);
-                assert.notEqual(output.stderr.toString().indexOf(err.message), -1);
-                assert.notEqual(output.status, 0);
-
-                done();
-            });
-        });
-
-        it('returns a helpful error when the config file contains a syntax error', function (done) {
-            Lapidus.loadConfig('./test/config/invalid.json', function (err) {
-                var output = spawnSync('node', ['index.js', '-c', './test/config/invalid.json']);
-
-                assert.notEqual(err, null);
-                assert.notEqual(output.stderr.toString().indexOf(err.message), -1);
-                assert.notEqual(output.status, 0);
-
-                done();
-            });
+            done();
         });
     });
+
+    it('returns a helpful error when we have insufficient permissions', function (done) {
+        // Git doesn't like us committing files that we don't have read permissions to
+        fs.writeFileSync('./test/config/forbidden.json', 'The Heart of the Island', { mode: 0o000, flag: 'w' });
+
+        Lapidus.loadConfig('./test/config/forbidden.json', function (err) {
+            var output = spawnSync('node', ['index.js', '-c', './test/config/forbidden.json']);
+
+            assert.notEqual(err, null);
+            assert.notEqual(output.stderr.toString().indexOf(err.message), -1);
+            assert.notEqual(output.status, 0);
+
+            fs.unlinkSync('./test/config/forbidden.json');
+
+            done();
+        });
+    });
+
+    it('returns a helpful error when the config file contains a syntax error', function (done) {
+        Lapidus.loadConfig('./test/config/invalid.json', function (err) {
+            var output = spawnSync('node', ['index.js', '-c', './test/config/invalid.json']);
+
+            assert.notEqual(err, null);
+            assert.notEqual(output.stderr.toString().indexOf(err.message), -1);
+            assert.notEqual(output.status, 0);
+
+            done();
+        });
+    });
+
+    it('returns the configuration file on success', function (done) {
+        Lapidus.loadConfig('./test/config/lapidus.json', function (err) {
+            var output = spawnSync('node', ['index.js', '-c', './test/config/lapidus.json']);
+
+            assert.ifError(err, null);
+            assert.equal(output.status, 0);
+
+            done();
+        });
+    });
+
 });
