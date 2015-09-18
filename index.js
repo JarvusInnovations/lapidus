51ff7da3717cd701da98cfe90c7d998881f159ce
Jeffrey Mealo <jeffreymealo@gmail.com>
2015-09-18 09:20:08 -0400

    Add -t (-test) flag to test configuration file and exit and stubs for mysql and postgresql workers

--- index.js
+++ index.js
@@ -7,9 +7,13 @@ var argv = require('yargs')
         .describe('c', 'Configuration file (JSON format)')
         .help('h')
         .alias('h', 'help')
+        .default('t', false)
+        .alias('t', 'test')
+        .describe('t', 'Test the configuration file and then exit')
         .argv,
 
     fs = require('fs'),
+    cluster = require('cluster'),
     jsonlint = require('jsonlint'),
 
     config;
@@ -28,19 +32,64 @@ function loadConfig(path, cb) {
             return cb(e, null);
         }
 
-        cb(null, data);
+        cb(validateConfig(data), data);
     });
+}
+
+function validateConfig(config) {
+    var errors = [],
+        error;
+
+    if (!Array.isArray(config.backends) || config.backends.length === 0) {
+        errors.push('Lapidus requires one or more backends to start.');
+    } else {
+        config.backends.forEach(function (backend, i) {
+            var workerFilename = `./lib/${backend.type}-worker.js`;
+
+            if (!fs.existsSync(workerFilename)) {
+                errors.push('Invalid backend type specified: ' + backend.type);
+            }
+        });
+    }
+
+    if (errors.length > 0) {
+        return new Error(errors.join('\n'));
+    }
+
+    return null;
 }
 
 if (!module.parent) {
     // Lapidus was invoked from the command line
     loadConfig(argv.config, function (err, configFile) {
+
         if (err) {
-            console.error('Unable to load configuration file: ' + err);
+            console.error('Error loading configuration file: ' + err);
             process.exit(1);
         }
 
         config = configFile;
+
+        if (argv.test) {
+            process.exit(0);
+        }
+
+        if (cluster.isMaster) {
+            config.backends.forEach(function (backend, i) {
+                var workerFilename = `./lib/${backend.type}-worker.js`,
+                    worker;
+
+                cluster.setupMaster({
+                    exec: workerFilename
+                });
+
+                worker = cluster.fork();
+
+                worker.on('online', function() {
+                    worker.send(JSON.stringify(backend));
+                });
+            });
+        }
     });
 } else {
     // Lapidus was required as a module
--- test/cli.test.js
+++ test/cli.test.js
@@ -3,13 +3,11 @@ var assert = require('assert'),
     spawnSync = require('child_process').spawnSync,
     fs = require('fs');
 
-Lapidus.loadConfig('./a-way-off-the-island.json');
-
 describe('Configuration File', function () {
 
     it('returns a helpful error when the file does not exist', function (done) {
         Lapidus.loadConfig('./test/config/a-way-off-the-island.json', function (err) {
-            var output = spawnSync('node', ['index.js', '-c', './test/config/a-way-off-the-island.json']);
+            var output = spawnSync('node', ['index.js', '-c', './test/config/a-way-off-the-island.json', '-t']);
 
             assert.notEqual(err, null);
             assert.notEqual(output.stderr.toString().indexOf(err.message), -1);
@@ -24,7 +22,7 @@ describe('Configuration File', function () {
         fs.writeFileSync('./test/config/forbidden.json', 'The Heart of the Island', { mode: 0o000, flag: 'w' });
 
         Lapidus.loadConfig('./test/config/forbidden.json', function (err) {
-            var output = spawnSync('node', ['index.js', '-c', './test/config/forbidden.json']);
+            var output = spawnSync('node', ['index.js', '-c', './test/config/forbidden.json', '-t']);
 
             assert.notEqual(err, null);
             assert.notEqual(output.stderr.toString().indexOf(err.message), -1);
@@ -38,7 +36,7 @@ describe('Configuration File', function () {
 
     it('returns a helpful error when the config file contains a syntax error', function (done) {
         Lapidus.loadConfig('./test/config/invalid.json', function (err) {
-            var output = spawnSync('node', ['index.js', '-c', './test/config/invalid.json']);
+            var output = spawnSync('node', ['index.js', '-c', './test/config/invalid.json', '-t']);
 
             assert.notEqual(err, null);
             assert.notEqual(output.stderr.toString().indexOf(err.message), -1);
@@ -50,10 +48,34 @@ describe('Configuration File', function () {
 
     it('returns the configuration file on success', function (done) {
         Lapidus.loadConfig('./test/config/lapidus.json', function (err) {
-            var output = spawnSync('node', ['index.js', '-c', './test/config/lapidus.json']);
+            var output = spawnSync('node', ['index.js', '-c', './test/config/lapidus.json', '-t']);
 
             assert.ifError(err, null);
             assert.equal(output.status, 0);
+
+            done();
+        });
+    });
+
+    it('returns a helpful error when no backends are configured', function (done) {
+        Lapidus.loadConfig('./test/config/no-backends.json', function (err) {
+            var output = spawnSync('node', ['index.js', '-c', './test/config/no-backends.json', '-t']);
+
+            assert.notEqual(err, null);
+            assert.notEqual(output.stderr.toString().indexOf(err.message), -1);
+            assert.notEqual(output.status, 0);
+
+            done();
+        });
+    });
+
+    it('returns a helpful error when a non-existant backend is configured', function (done) {
+        Lapidus.loadConfig('./test/config/invalid-backend.json', function (err) {
+            var output = spawnSync('node', ['index.js', '-c', './test/config/invalid-backend.json', '-t']);
+
+            assert.notEqual(err, null);
+            assert.notEqual(output.stderr.toString().indexOf(err.message), -1);
+            assert.notEqual(output.status, 0);
 
             done();
         });
