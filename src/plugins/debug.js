eeb793e94fe68acbc72df21f83c5c3c8ef799485
Jeffrey Mealo <jeffreymealo@gmail.com>
2016-06-13 10:26:14 -0400

    Add all event types to debug plugin

--- src/plugins/debug.js
+++ src/plugins/debug.js
@@ -1,17 +1,36 @@
 module.exports = {
     init: function (config, eventEmitter) {
         if (config.enabled) {
-            console.log('Debug: init called');
+            console.log('Debug: plugin init called; attaching event listeners');
 
             eventEmitter.on('event', function(event) {
-                console.log('Debug: event called');
+                console.log('Debug: event emitted');
+                console.log(event);
+            });
+
+            eventEmitter.on('update', function(event) {
+                console.log('Debug: update emitted');
+                console.log(event);
+            });
+            eventEmitter.on('insert', function(event) {
+                console.log('Debug: insert emitted');
+                console.log(event);
+            });
+
+            eventEmitter.on('delete', function(event) {
+                console.log('Debug: delete emitted');
+                console.log(event);
+            });
+
+            eventEmitter.on('schema', function(event) {
+                console.log('Debug: schema emitted');
                 console.log(event);
             });
         }
     },
 
     validateConfig: function (config, scopeConfig, globalConfig) {
-        console.log('Debug: validateConfig called');
+        console.log('Debug: validateConfig called; (no-op)');
         return [];
     }
 };
