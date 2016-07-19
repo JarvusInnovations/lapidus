198503b855d83abd8152ba701505269bd1530b91
Jeffrey Mealo <jeffreymealo@gmail.com>
2016-07-19 10:27:31 -0400

    Travis: Try moving database and role creation to postgresql_setup.sql script and executing from .travis.yml

--- test/sql/postgresql_setup.sql
+++ test/sql/postgresql_setup.sql
@@ -1,18 +1,28 @@
-\set ON_ERROR_STOP true
 SET client_min_messages TO WARNING;
 
-SELECT pg_drop_replication_slot(slot_name) FROM pg_replication_slots;
+-- Drop existing logical replication slots
+SELECT pg_drop_replication_slot(slot_name)
+  FROM pg_replication_slots
+ WHERE database = 'lapidus'
+    OR database = 'hurley';
+
+-- Drop existing databases
+DROP DATABASE IF EXISTS lapidus;
+DROP DATABASE IF EXISTS hurley;
+
+-- Create lapidus role and database
+CREATE ROLE lapidus PASSWORD '2PQM9aiKMJX5chv76gYdFJNi' SUPERUSER CREATEDB CREATEROLE INHERIT LOGIN;
+CREATE DATABASE lapidus OWNER lapidus;
+
+-- Create hurley role and database
+CREATE ROLE hurley PASSWORD '2PQM9aiKMJX5chv76gYdFJNi' SUPERUSER CREATEDB CREATEROLE INHERIT LOGIN;
+CREATE DATABASE hurley OWNER hurley;
 
 \c lapidus;
 
-DO $$
-  BEGIN
-    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'sex') THEN
-      CREATE TYPE sex AS ENUM ('M', 'F');
-    END IF;
-END$$;
+CREATE TYPE sex AS ENUM ('M', 'F');
 
-CREATE TABLE IF NOT EXISTS "test_table" (
+CREATE TABLE "test_table" (
   "id" serial NOT NULL PRIMARY KEY,
   "first_name" varchar(100) NOT NULL,
   "last_name" varchar(200) NOT NULL,
@@ -23,14 +33,9 @@ CREATE TABLE IF NOT EXISTS "test_table" (
  
 \c hurley;
 
-DO $$
-  BEGIN
-    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'sex') THEN
-      CREATE TYPE sex AS ENUM ('M', 'F');
-    END IF;
-END$$;
+CREATE TYPE sex AS ENUM ('M', 'F');
 
-CREATE TABLE IF NOT EXISTS "test_table" (
+CREATE TABLE "test_table" (
   "id" serial NOT NULL PRIMARY KEY,
   "first_name" varchar(100) NOT NULL,
   "last_name" varchar(200) NOT NULL,
